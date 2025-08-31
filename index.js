195b4f228470b4b5948ababeb16c2b0019051773
pauriyalmohit <161916772+pauriyalmohit@users.noreply.github.com>
2025-08-31 23:42:42 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,79 +1,71 @@
-const express = require("express");
+ const express = require("express");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 const methodOverride = require("method-override");
-
 const app = express();
 
-app.set("view engine", "ejs");
-app.use(express.urlencoded({ extended: true }));
+
+app.set("view engine","ejs");
+app.use(express.urlencoded({extended: true}));
 app.use(express.json());
 app.use(express.static("public"));
 app.use(methodOverride("_method"));
-
 
 mongoose.connect('mongodb+srv://mohitpouriyalmonu7088:ownUzLi1VsCF9O4f@cluster0.vcu2vvv.mongodb.net/',{
   useNewUrlParser:true,
   useUnifiedTopology:true
 })
-.then(()=> console.log("MongoDB Atlas connected"))
+.then(()=>console.log("MongoDB Atlas connected"))
 .catch(err=> console.error("connection error:",err));
 
 const taskSchema = new mongoose.Schema({
-  name: String
+  name: string
 });
 
-const Task = mongoose.model("Task", taskSchema);
+const Task = mongoose.model("Task",taskSchema);
 
-// Default tasks
-const defaultTasks = [
-  new Task({ name: "Learn DSA" }),
-  new Task({ name: "Complete Project" }),
-  new Task({ name: "Read Documentation" })
+
+const defaultTasks =[
+  new Task({name: "Learn DSA"}),
+  new Task({name:"complete Project"}),
+  new Task({name:"Real Documentation"})
 ];
 
-app.get("/", async (req, res) => {
-  try {
+app.get("/",async(req,res)=>{
+  try{
     const tasks = await Task.find();
 
-    // Insert default tasks only if DB is empty
-    if (tasks.length === 0) {
+    if(tasks.length ===0){
       await Task.insertMany(defaultTasks);
       return res.redirect("/");
     }
-
-    res.render("list", { dayej: tasks });
-  } catch (err) {
+    res.render("list",{dayej: tasks});
+  } catch (err){
     console.log(err);
     res.status(500).send("Error loading tasks");
   }
 });
 
-// Add new task
-app.post("/", async (req, res) => {
+app.post("/",async (req,res)=>{
   const itemName = req.body.elel;
-  const newTask = new Task({ name: itemName });
-  await newTask.save();
+  const newTask = new Task({name: itemName});
+  await newTask.save("/");
   res.redirect("/");
 });
 
-// Delete task
-app.delete("/delete/:id", async (req, res) => {
-  const id = req.params.id;
+app.delete("/delete/:id",async (req,res)=>{
+  const id= req.params.id;
   await Task.findByIdAndDelete(id);
   res.redirect("/");
 });
 
-// Update task
-app.put("/update/:id", async (req, res) => {
-  const id = req.params.id;
-  const newName = req.body.name;
-  await Task.findByIdAndUpdate(id, { name: newName });
+app.put("/update/:id",async(req,res)=>{
+  const id =req.params.id;
+  const newName= req.body.name;
+  await Task.findByIdAndUpdate(id,{name: newName});
   res.redirect("/");
 });
 
-
-app.listen(3000, () => {
-  console.log("Server running on http://localhost:3000");
+app.listen(3000,()=>{
+  console.log("server running on http://localhost:3000");
 });
-
